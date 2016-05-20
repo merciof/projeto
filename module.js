eae89d0c4a2549d842a5cf043f96440b9cc6e0ad
merciof <merciofilho@gmail.com>
2016-05-20 14:06:52 -0300

    atualização dos links da navbar e do livros.json

--- module.js
+++ module.js
@@ -1,22 +1,23 @@
 var app = angular.module("app", []);
 
-app.controller("controller", ['$scope', function ($scope) {
-    $scope.objectArray = [{
+app.controller('controller', ['$scope','$http', function ($scope,$http) {
+    
+    /*$scope.objectArray = [{
         nome: "Java Como Programar",
         edicao: "Terceira",
         sinopse: "blablabla",
         autor: "Deitel",
         estado: "semi noovo"
-    }];
+    }];*/
 
-    $scope.SubmitForm = function () {
-        this.objectArray.push({
-            nome: this.Object.nome,
-            edicao: this.Object.edicao,
-            sinopse: this.Object.sinopse,
-            autor: this.Object.autor,
-            estado: this.Object.estado
-        });
-    }
+    $http.get("livros.json").success(function (result) {
+   $scope.objectArray = result;
+   }).error(function (error) {
+   alert(error.error + "/" + error.statusCode);
+   
+    });
+    
+    
+  
 
 }]);
