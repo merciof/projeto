f781173ac145e8e7ec9b5f282828463fa543bc20
merciof <merciofilho@gmail.com>
2016-06-27 01:45:53 -0300

    integração com o frontend

---
+++ BackEnd/das-boot/target/classes/public/module.js
@@ -0,0 +1,30 @@
+var app = angular.module("app", []);
+
+app.controller('controller', ['$scope', '$http', function ($scope, $http) {
+
+    /*$scope.objectArray = [{
+        nome: "Java Como Programar",
+        edicao: "Terceira",
+        sinopse: "blablabla",
+        autor: "Deitel",
+        estado: "semi noovo"
+    }];*/
+
+    $http.get("livros.json").success(function (result) {
+        $scope.objectArray = result;
+    }).error(function (error) {
+        alert(error.error + "/" + error.statusCode);
+    });
+
+
+    $scope.SubmitForm = function () {
+        this.objectArray.push({
+            nome: this.Object.nome,
+            edicao: this.Object.edicao,
+            sinopse: this.Object.sinopse,
+            autor: this.Object.autor,
+            estado: this.Object.estado
+        });
+    }
+
+}]);
